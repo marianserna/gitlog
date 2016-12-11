9b78303428809912ddd801c5e04cc5cc2ac60346
Marian <marianhalliday@gmail.com>
2016-12-10 19:24:12 -0500

    Configuring firebase to work with React + Showing favourites in real time

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import * as firebase from 'firebase';
 
 class App extends React.Component {
   componentWillMount() {
     this.setState({
       username: localStorage.username,
       results: [
-        {"id":123,"name":"Perrito","owner":"Joroman","html_url":"https://github.com/Joroman/Perrito","description":"Identificacion de colores","stars":1,"language":"Java"},
-        {"id":555,"name":"Perrito","owner":"Joroman","html_url":"https://github.com/Joroman/Perrito","description":"Identificacion de colores","stars":1,"language":"Java"},
-        {"id":908,"name":"Perrito","owner":"Joroman","html_url":"https://github.com/Joroman/Perrito","description":"Identificacion de colores","stars":1,"language":"Java"}
-      ]
+      ],
+      favourites: []
     });
+    this.initializeFirebase();
+
+    // https://firebase.google.com/docs/database/web/read-and-write
+    this.database.child('favourites').on('value', function(snapshot) {
+      // data = object with results (also objects)
+      const data = snapshot.val();
+      // Transform data into array
+      const favourites = Object.values(data);
+      this.setState({
+        favourites: favourites
+      });
+    }.bind(this));
+  }
+
+  // Firebase initialization (Code from firebase.com + sweet installation tutorial: http://blog.tylerbuchea.com/create-react-app-firebase-for-prototyping/)
+  initializeFirebase() {
+    const config = {
+      apiKey: "AIzaSyBvZjmp8k0n1IF-R7cyp6snpFIPI1PENxA",
+      authDomain: "gitlog-13c88.firebaseapp.com",
+      databaseURL: "https://gitlog-13c88.firebaseio.com",
+      storageBucket: "gitlog-13c88.appspot.com",
+      messagingSenderId: "200533762278"
+    };
+    this.database = firebase
+      .initializeApp(config)
+      .database()
+      .ref();
   }
 
   render() {
@@ -19,6 +45,10 @@ class App extends React.Component {
 
         <section>
           {this.renderResults()}
+        </section>
+
+        <section>
+          {this.renderFavourites()}
         </section>
       </div>
     )
@@ -39,7 +69,7 @@ class App extends React.Component {
 
   renderResults() {
     // I have an array of objects but I want to show an array of HTML
-    return this.state.results.map(function(repo) {
+    return this.state.results.map((repo) => {
       return(
         <div className="repo" key={repo.id}>
           <h3>{repo.name}</h3>
@@ -47,9 +77,27 @@ class App extends React.Component {
           <p>{repo.description}</p>
           <p><a href={repo.html_url}>VISIT REPO</a></p>
           <p>Stars {repo.stars}</p>
+          <p><a href="#" onClick={(e) => {e.preventDefault(); this.star(repo)}}>STAR</a></p>
         </div>
       )
     });
+  }
+
+  renderFavourites() {
+    return this.state.favourites.map(function(fav) {
+      return(
+        <a className="fav" key={fav.id} href={fav.html_url}>
+          <h3>{fav.added_by} has faved {fav.name}</h3>
+          <p>{fav.language}</p>
+        </a>
+      )
+    });
+  }
+
+  star(repo) {
+    repo['added_by'] = this.state.username;
+    // console.log(repo);
+    this.database.child('favourites').push(repo);
   }
 
   search(e) {
